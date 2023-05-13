429ae0085896b7f275c7a5402311d405fa1f8189
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-05-13 21:57:35 +0700

    Edit footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
       <section id="footer" className="footer fixed-bottom">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-12 d-flex justify-content-center align-items-center mb-2">
-              <p className="text-primary">
+            <div className="col-12 bg-secondary d-flex justify-content-center align-items-center pb-2">
+              <p className="text-primary text-center">
                 © 2023 - dibimbing Bootcamp Final Project by{' '}
-                <a href="https://www.linkedin.com/in/arifiensr/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/arifiensr/" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                   <span className="fw-bold d-inline-flex align-items-center">
                     M. Arifien Syachrizal <i className="bx bxl-linkedin-square ps-1 fs-5"></i>
                   </span>
