c406742d04301f0fc2d997d71346899319fdee1c
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-06-01 10:58:44 +0700

    Edit footerlink

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
           <div className="row">
             <div className="col-12 bg-secondary d-flex justify-content-center align-items-center py-2">
               <p className="text-primary text-center">
-                © 2023 - dibimbing Bootcamp Final Project by{' '}
+                © 2023 - <a href="https://dibimbing.id/" target="_blank" rel="noopener noreferrer" className="text-decoration-none fw-bold">dibimbing.id</a> Bootcamp Final Project by{' '}
                 <a href="https://arifiensr.netlify.app/" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
                   <span className="fw-bold d-inline-flex align-items-center">
                     M. Arifien Syachrizal
